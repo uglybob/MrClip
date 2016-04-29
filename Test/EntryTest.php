29c63e73775227d543565dfdb32480c4d1a4d903
Sebastian Reinhold <sebastian@bitbernd.de>
2016-04-29 15:09:20 +0200

    add entry class tests

--- Test/EntryTest.php
+++ Test/EntryTest.php
@@ -2,6 +2,8 @@
 
 namespace Uglybob\MrClip\Test;
 
+use Uglybob\MrClip\Lib\Entry;
+
 class EntryTest extends \PhpUnit_Framework_TestCase
 {
     // {{{ setUp
@@ -41,6 +43,57 @@ class EntryTest extends \PhpUnit_Framework_TestCase
     public function testGetText()
     {
         $this->assertSame('testText', $this->entry->getText());
+    }
+    // }}}
+
+    // {{{ testSetId
+    public function testSetId()
+    {
+        $this->assertSame(42, $this->entry->getId());
+        $this->entry->SetId(1);
+        $this->assertSame(1, $this->entry->getId());
+    }
+    // }}}
+
+    // {{{ testGetDefaults
+    public function testGetDefaults()
+    {
+        $default = new EntryTestClass();
+
+        $this->assertNull($default->getId());
+        $this->assertNull($default->getActivity());
+        $this->assertNull($default->getCategory());
+        $this->assertEquals([], $default->getTags());
+        $this->assertNull($default->getText());
+    }
+    // }}}
+
+    // {{{ testGetActigory
+    public function testGetActigory()
+    {
+        $this->assertSame('testActivity@testCategory', $this->entry->getActigory());
+    }
+    // }}}
+    // {{{ testFormat
+    public function testFormat()
+    {
+        $this->assertSame('testActivity@testCategory +testTag1 +testTag2 testText', $this->entry->format());
+    }
+    // }}}
+
+    // {{{ testGetFormattedTags
+    public function testGetFormattedTags()
+    {
+        $this->assertSame('+testTag1 +testTag2', $this->entry->getFormattedTags());
+    }
+    // }}}
+
+    // {{{ testFormatTags
+    public function testFormatTags()
+    {
+        $this->assertSame('+testTag1', Entry::formatTags(['testTag1']));
+        $this->assertSame('+testTag1 +testTag2', Entry::formatTags(['testTag1', 'testTag2']));
+        $this->assertSame('', Entry::formatTags(null));
     }
     // }}}
 }
